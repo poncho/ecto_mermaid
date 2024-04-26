7954567e5b9fba78d1f1c472885aa33b55f6dcd5
poncho <poncho.811@gmail.com>
2024-04-26 01:54:43 -0600

    Adds specs to the writer file
    Also adds a tab function to add tabs anywhere with a duplicate number so
    we don't have to manually write multiple tab characters when writing to the file

--- lib/ecto_mermaid/writer.ex
+++ lib/ecto_mermaid/writer.ex
@@ -1,12 +1,15 @@
 defmodule EctoMermaid.Writer do
+  @spec open_mermaid(File.io_device()) :: :ok
   def open_mermaid(file) do
     IO.write(file, "```mermaid\n")
   end
 
+  @spec close_mermaid(File.io_device()) :: :ok
   def close_mermaid(file) do
     IO.write(file, "```")
   end
 
+  @spec add_title(File.io_device(), String.t()) :: :ok
   def add_title(file, title) do
     title = """
     ---
@@ -19,16 +22,27 @@ defmodule EctoMermaid.Writer do
     IO.write(file, title)
   end
 
+  @spec draw_table(File.io_device(), String.t(), [{String.t(), String.t()}]) :: :ok
   def draw_table(file, table_name, columns) do
-    buffer = "\t#{table_name} {\n"
+    buffer = "#{tab(1)}#{table_name} {\n"
 
     buffer =
       Enum.reduce(columns, buffer, fn {column_name, column_type}, acc ->
-        acc <> "\t\t#{column_type} #{column_name}\n"
+        acc <> "#{tab(2)}#{column_type} #{column_name}\n"
       end)
 
-    buffer = buffer <> "}\n\n"
+    buffer = buffer <> "#{tab(1)}}\n\n"
 
     IO.write(file, buffer)
+  end
+
+  # It should be a macro tbh
+  @spec tab(integer()) :: String.t()
+  defp tab(1) do
+    "\t"
+  end
+
+  defp tab(tab_level) do
+    String.duplicate("\t", tab_level)
   end
 end
