9856135b39bca9a00036ad880683d16e14796c34
poncho <poncho.811@gmail.com>
2024-05-04 23:59:14 -0600

    Adds hex publishing info

--- mix.exs
+++ mix.exs
@@ -5,9 +5,12 @@ defmodule EctoMermaid.MixProject do
     [
       app: :ecto_mermaid,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/poncho/ecto_mermaid"
     ]
   end
 
@@ -23,6 +26,20 @@ defmodule EctoMermaid.MixProject do
     [
       {:ecto_sql, "~> 3.0"},
       {:ex_doc, "~> 0.32", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "Ecto extension to create Entity Relationship Diagrams using Mermaid"
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/poncho/ecto_mermaid",
+        "Mermaid" => "https://mermaid.js.org"
+      }
     ]
   end
 end
